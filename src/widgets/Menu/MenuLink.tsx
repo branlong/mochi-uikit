ea44d4075cc9e180d7cd1972c486d88a291e412f
frankydev1 <72719344@users.noreply.github.com>
2021-04-14 00:05:37 -0400

    updates

--- src/widgets/Menu/MenuLink.tsx
+++ src/widgets/Menu/MenuLink.tsx
@@ -3,13 +3,13 @@ import { NavLink } from "react-router-dom";
 
 const MenuLink: React.FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({ href, target, ...otherProps }) => {
   const isHttpLink = href?.startsWith("http");
-  const targetBlank = "target=_blank";
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const Tag: any = isHttpLink ? "a" : NavLink;
   const props = isHttpLink ? { href } : { to: href };
   // if(target==="_blank"){
   //   targetBlank = "target=_blank"
   // }
+  const targetBlank = "target=_blank";
   return <Tag {...props} {...otherProps} {...targetBlank} />;
 };
 
